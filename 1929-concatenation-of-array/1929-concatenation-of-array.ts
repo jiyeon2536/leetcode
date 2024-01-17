7bc0751ab9a58e7f95deabe1cd1c70b4f2280ee4
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-01-17 22:45:31 +0900

    Time: 64 ms (86.05%), Space: 47.3 MB (11.63%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.ts
@@ -0,0 +1,9 @@
+function getConcatenation(nums: number[]): number[] {
+    const answer = []
+    for (let i=0; i < 2; i++) {
+    nums.map((num)=> {
+        answer.push(num)
+    })
+    }
+    return answer
+};
