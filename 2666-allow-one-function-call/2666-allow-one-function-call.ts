08ec141c7e3b6b11d31f9d3f04c760de451930c0
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-01-28 14:09:44 +0900

    Time: 58 ms (46.60%), Space: 51.1 MB (5.58%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,24 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let call = false;
+
+    return function (...args) {
+        if (!call) {
+            const result = fn(...args)
+            call = !call
+            return result
+        } else {
+            return undefined
+        }
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
